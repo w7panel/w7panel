4ce317d0557ddf6f342c47e08f0a27a281551cb9
fykyx521 <446897682@qq.com>
2026-02-28 11:05:54 +0000

    fix: microapp proxy

--- app/application/provider.go
+++ app/application/provider.go
@@ -224,7 +224,7 @@ func (p Provider) RegisterHttpRoutes(server *httpserver.Server) {
 		engine.GET("/panel-api/v1/noauth/site/init-user", controller2.Site{}.InitUser)
 
 		engine.GET("/panel-api/v1/microapp/top", middleware.Auth{}.Process, controller2.MicroApp{}.List)                     //获取microapp列表
-		engine.GET("/panel-api/v1/microapp/:name/proxy/*path", middleware.Auth{}.Process, controller2.Proxy{}.ProxyMicroApp) //获取microapp列表
+		engine.Any("/panel-api/v1/microapp/:name/proxy/*path", middleware.Auth{}.Process, controller2.Proxy{}.ProxyMicroApp) //microapp proxy
 
 	})
 }
