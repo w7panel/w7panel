7d49c53198e6bc829167ea982b61f8c1f1566b08
fykyx521 <446897682@qq.com>
2026-03-05 08:58:53 +0000

    fix: volume volumes

--- app/zpk/logic/helmchart.go
+++ app/zpk/logic/helmchart.go
@@ -115,23 +115,24 @@ func fillHelmSet(packageApp *types.PackageApp, childName string, ignore []string
 	}
 
 	set += " --set " + "replicas=" + strconv.Itoa(int(packageApp.Replicas))
-	// 正常应该 完全交给helm 外部不干预 只提供PVC_NAME 目前因为subPath问题，需要直接传volumes volumeMounts
-	if packageApp.GetVolumeMounts() != nil && len(packageApp.GetVolumeMounts()) > 0 {
-		jsonstr, err := helper.ToJson(packageApp.GetVolumeMounts())
-		if err != nil {
-			slog.Error("helm install job", "error", err)
-		} else {
-			set += " --set-json '" + childName + "volumeMounts=" + jsonstr + "'"
-		}
-	}
-	if packageApp.GetVolumes() != nil && len(packageApp.GetVolumes()) > 0 {
-		jsonstr, err := helper.ToJson(packageApp.GetVolumes())
-		if err != nil {
-			slog.Error("helm install job", "error", err)
-		} else {
-			set += " --set-json '" + childName + "volumes=" + jsonstr + "'"
-		}
-	}
+	// 正常应该 完全交给helm 外部不干预 只提供PVC_NAME 子应用无法获取原始volume volumes 配置
+	// 目前因为subPath问题，需要直接传volumes volumeMounts
+	// if packageApp.GetVolumeMounts() != nil && len(packageApp.GetVolumeMounts()) > 0 {
+	// 	jsonstr, err := helper.ToJson(packageApp.GetVolumeMounts())
+	// 	if err != nil {
+	// 		slog.Error("helm install job", "error", err)
+	// 	} else {
+	// 		set += " --set-json '" + childName + "volumeMounts=" + jsonstr + "'"
+	// 	}
+	// }
+	// if packageApp.GetVolumes() != nil && len(packageApp.GetVolumes()) > 0 {
+	// 	jsonstr, err := helper.ToJson(packageApp.GetVolumes())
+	// 	if err != nil {
+	// 		slog.Error("helm install job", "error", err)
+	// 	} else {
+	// 		set += " --set-json '" + childName + "volumes=" + jsonstr + "'"
+	// 	}
+	// }
 	if fillfullName {
 		set += " --set " + childName + "fullnameOverride=" + packageApp.GetName()
 	}
