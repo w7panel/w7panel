dc9683ac02f49fa6cffbbe5b427378dbc9df759b
fykyx521 <446897682@qq.com>
2026-03-19 11:27:18 +0000

    fix: pid

--- app/application/http/controller/pid.go
+++ app/application/http/controller/pid.go
@@ -4,6 +4,7 @@ import (
 
 	// "github.com/we7coreteam/w7-rangine-go/v2/pkg/support/facade"
 
+	"gitee.com/we7coreteam/k8s-offline/common/service/k8s/pid"
 	"github.com/gin-gonic/gin"
 	"github.com/we7coreteam/w7-rangine-go/v2/src/http/controller"
 )
@@ -12,8 +13,23 @@ type Pid struct {
 	controller.Abstract
 }
 
-func (self Pid) List(http *gin.Context) {
-
+func (self Pid) GetPid(http *gin.Context) {
+	params := pid.PidParam{}
+	if !self.Validate(http, &params) {
+		return
+	}
+	token := http.MustGet("k8s_token").(string)
+	pidObj, err := pid.NewPid(token)
+	if err != nil {
+		self.JsonResponseWithoutError(http, err)
+		return
+	}
+	result, err := pidObj.Handle(params)
+	if err != nil {
+		self.JsonResponseWithoutError(http, err)
+		return
+	}
+	self.JsonResponseWithoutError(http, result.ToArray())
 }
 
 func (self Pid) EtcPasswd(http *gin.Context) {
