f8dc9d305e88e90c2a6eca16c144cec5ede5204e
fykyx521 <446897682@qq.com>
2026-03-19 11:27:18 +0000

    fix: pid

--- common/service/k8s/pid/types.go
+++ common/service/k8s/pid/types.go
@@ -1,5 +1,11 @@
 package pid
 
+import (
+	"strconv"
+
+	corev1 "k8s.io/api/core/v1"
+)
+
 var pidCache = make(map[string]string)
 
 func cachePutPid(containerId string, pid string) {
@@ -19,7 +25,41 @@ type PidParam struct {
 }
 
 type PidResult struct {
-	Pid     int    `json:"pid"`
-	SubPid  int    `json:"subPid"`
-	ProxyIp string `json:"proxyIp"`
+	Pid      int    `json:"pid"`
+	SubPid   int    `json:"subPid"`
+	ProxyIp  string `json:"proxyIp"`
+	AgentPod *corev1.Pod
+}
+
+func (p *PidResult) ToArray() map[string]string {
+	podIp := p.ProxyIp
+	pidstr := strconv.Itoa(p.Pid)
+	subpidstr := strconv.Itoa(p.SubPid)
+	webdavUrl := "/panel-api/v1/" + podIp + ":8000/proxy/panel-api/v1/files/webdav-agent/" + pidstr + "/agent"
+	webdavBasePath := "panel-api/v1/files/webdav-agent/" + pidstr + "/agent" //前端根据这个过滤掉 当前目录?
+	compressUrl := "/panel-api/v1/" + podIp + ":8000/proxy/panel-api/v1/files/compress-agent/" + pidstr
+	permissionUrl := "/panel-api/v1/" + podIp + ":8000/proxy/panel-api/v1/files/permission-agent/" + pidstr
+	if p.SubPid > 0 {
+		webdavUrl = "/panel-api/v1/" + podIp + ":8000/proxy/panel-api/v1/files/webdav-agent/" + pidstr + "/subagent/" + subpidstr + "/agent"
+		webdavBasePath = "panel-api/v1/files/webdav-agent/" + pidstr + "/subagent/" + subpidstr + "/agent"
+		compressUrl = "/panel-api/v1/" + podIp + ":8000/proxy/panel-api/v1/files/compress-agent/" + pidstr + "/subagent/" + subpidstr
+		permissionUrl = "/panel-api/v1/" + podIp + ":8000/proxy/panel-api/v1/files/permission-agent/" + pidstr + "/subagent/" + subpidstr
+	}
+	pod := p.AgentPod
+	return map[string]string{
+		"podName":       pod.Name,
+		"pid":           pidstr,
+		"subPid":        subpidstr,
+		"namespace":     pod.Namespace,
+		"containerName": pod.Spec.Containers[0].Name,
+		"podIp":         podIp,
+		// "pwd":            pwd,
+		"webdavUrl": webdavUrl,
+		// "webdavToken":    token,
+		"webdavBasePath": webdavBasePath,
+		"compressUrl":    compressUrl,
+		"permissionUrl":  permissionUrl,
+		// "users":          users,
+	}
+
 }
